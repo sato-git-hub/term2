669a74f32ef4069a095c2eb1f097a612bada6de7
sato-git-hub <mimilili.disney@gmail.com>
2025-11-04 14:03:15 +0900

    ターム２の提出クエストを提出

---
+++ README.md
@@ -0,0 +1,106 @@
+##　手順
+1.リポジトリをクローン
+ - GitHubのこのリポジトリーをクローンする
+```bash
+    git clone このリポジトリのurl
+```
+ - クローンしたディレクトリに移動
+ ```bash 
+  cd term2
+ ```
+
+2.MySQLの起動
+```bash
+    mysql -u ユーザー名 -p 
+```
+
+3.データベースの作成
+ - internet_tv_dbという名前で、データベースを作ります
+ ```sql
+    CREATE DATABASE internet_tv_db;
+  ```
+ - 作成したデータベースを選択
+ ```sql
+    USE internet_tv_db;
+  ```
+ - MySQLを終了
+```sql
+    QUIT;
+  ```
+
+4.テーブルの構築
+```bash
+    mysql -u ユーザー名 -p internet_tv_db < create_tables.sql
+```
+5.テーブルにサンプルデータを入れる
+```bash
+    mysql -u ユーザー名 -p internet_tv_db < insert_data.sql
+    ```
+
+## テーブル設計
+
+- channels
+| カラム名 | データ型 |NULL |キー|初期値|AUTO INCREMENT|
+|-|-|-|-|-|-|
+| channel_id| int | |PRIMARY||YES|
+|  channel_name |  varchar(100)| |UNIQUE|||
+
+- shows
+| カラム名 | データ型 |NULL |キー|初期値|AUTO INCREMENT|
+|-|-|-|-|-|-|
+| show_id| int | |PRIMARY||YES|
+| genre_id |int| |FOREIGN, UNIQUE(uk_genre_show)|||
+| show_name |varchar(100)|UNIQUE(uk_genre_show)||||
+| show_detail |varchar(200)|NULL||||
+
+- genres
+| カラム名 | データ型 |NULL |キー|初期値|AUTO INCREMENT|
+|-|-|-|-|-|-|
+| genre_id| int | |PRIMARY||YES|
+| genre_name | varchar(100)| |UNIQUE|||
+
+- seasons
+| カラム名 | データ型 |NULL |キー|初期値|AUTO INCREMENT|
+|-|-|-|-|-|-|
+|season_id| int | |PRIMARY||YES|
+|show_id | int| |FOREIGN, UNIQUE(uk_show_season)|||
+| season_number |int| |UNIQUE(uk_show_season)|||
+
+- episodes
+| カラム名 | データ型 |NULL |キー|初期値|AUTO INCREMENT|
+|-|-|-|-|-|-|
+| episode_id | int | |PRIMARY||YES|
+| show_id | int| |FOREIGN,UNIQUE(uk_show_episode)|||
+| season_id |int|NULL|FOREIGN|||
+| schedule_id |int| |FOREIGN|||
+| episode_number |  int| |UNIQUE(uk_show_episode)|||
+| episode_name |  varchar(100)| ||||
+| viewing_time | time| ||||
+| view |  bigint(20)| ||0||
+| episode_detail |  varchar(200)|NULL||||
+
+- schedules
+| カラム名 | データ型 |NULL |キー|初期値|AUTO INCREMENT|
+|-|-|-|-|-|-|
+|schedule_id| int | |PRIMARY||YES|
+| start_time| DATETIME| |UNIQUE(uk_schedule_time)|||
+| end_time| DATETIME||UNIQUE(uk_schedule_time)|||
+
+- program_schedules
+| カラム名 | データ型 |NULL |キー|初期値|AUTO INCREMENT|
+|-|-|-|-|-|-|
+|program_schedule_id| int | |PRIMARY||YES|
+|episode_id| int| |FOREIGN, UNIQUE(uk_episode_schedule)|||
+|schedule_id| int| |FOREIGN, UNIQUE(uk_episode_schedule)|||
+
+- program_channels
+| カラム名 | データ型 |NULL |キー|初期値|AUTO INCREMENT|
+|-|-|-|-|-|-|
+|program_channel_id| int | |PRIMARY||YES|
+|episode_id| int| |FOREIGN, UNIQUE(uk_episode_channel)|||
+|channel_id| int| |FOREIGN, UNIQUE(uk_episode_channel)|||
+
+
+
+
+
---
+++ create_tables.sql
@@ -0,0 +1,90 @@
+CREATE DATABASE internet_tv;
+
+CREATE TABLE channels (
+    channel_id INT NOT NULL AUTO_INCREMENT,  
+    channel_name VARCHAR(100) NOT NULL,        
+    PRIMARY KEY(channel_id),
+    UNIQUE KEY(channel_name)                    
+);
+
+CREATE TABLE shows (
+    show_id INT NOT NULL AUTO_INCREMENT, 
+    genre_id INT NOT NULL,   
+    show_name VARCHAR(100) NOT NULL,  
+    show_detail VARCHAR(200) NULL,      
+    PRIMARY KEY(show_id), 
+    FOREIGN KEY (genre_id) REFERENCES genres(genre_id)   
+    
+    UNIQUE KEY uk_genre_show (genre_id, show_name)                 
+);
+
+CREATE TABLE genres (
+    genre_id INT NOT NULL AUTO_INCREMENT,  
+    genre_name VARCHAR(100) NOT NULL,    
+    PRIMARY KEY(genre_id),
+    UNIQUE KEY(genre_name) 
+);
+
+CREATE TABLE seasons (
+		season_id INT NOT NULL AUTO_INCREMENT, 
+    show_id INT NOT NULL, 
+    season_number INT NOT NULL,  
+		PRIMARY KEY(season_id),
+		FOREIGN KEY (show_id) REFERENCES shows(show_id)   
+		UNIQUE KEY uk_show_season (show_id, season_number)
+);
+
+CREATE TABLE episodes (
+    episode_id INT NOT NULL AUTO_INCREMENT,
+    show_id INT NOT NULL,
+    season_id INT NULL,                      
+    schedule_id INT NOT NULL,                
+    episode_number INT NOT NULL,             
+    episode_name VARCHAR(100) NOT NULL,
+    viewing_time TIME NOT NULL,         
+    view BIGINT NOT NULL DEFAULT 0,         
+    episode_detail VARCHAR(200) NULL,             
+		PRIMARY KEY(episode_id),
+    
+    FOREIGN KEY (show_id) REFERENCES shows(show_id),
+    FOREIGN KEY (season_id) REFERENCES seasons(season_id),
+    FOREIGN KEY (schedule_id) REFERENCES schedules(schedule_id),
+
+    UNIQUE KEY uk_show_episode (show_id, episode_number)
+);
+
+CREATE TABLE schedules (
+    schedule_id INT NOT NULL AUTO_INCREMENT,
+    start_time DATETIME NOT NULL,
+    end_time DATETIME NOT NULL,
+    
+    PRIMARY KEY(schedule_id),
+
+    UNIQUE KEY uk_schedule_time (start_time, end_time)
+);
+
+CREATE TABLE program_schedules (
+    program_schedule_id INT NOT NULL AUTO_INCREMENT,
+    episode_id INT NOT NULL,
+    schedule_id INT NOT NULL,
+
+    PRIMARY KEY(program_schedule_id),
+    
+    UNIQUE KEY uk_episode_schedule (episode_id, schedule_id),
+
+    FOREIGN KEY (episode_id) REFERENCES episodes(episode_id),
+    FOREIGN KEY (schedule_id) REFERENCES schedules(schedule_id)
+);
+
+CREATE TABLE program_channels (
+    program_channel_id INT NOT NULL AUTO_INCREMENT,
+    episode_id INT NOT NULL,
+    channel_id INT NOT NULL,
+
+    PRIMARY KEY(program_channel_id),
+    
+    UNIQUE KEY uk_episode_channel (episode_id, channel_id),
+
+    FOREIGN KEY (episode_id) REFERENCES episodes(episode_id),
+    FOREIGN KEY (channel_id) REFERENCES channels(channel_id)
+);
